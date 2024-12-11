28d6b3d4cf83f6ac108da668f8555d986aa1fbe4
Luke Paoloni <lukepaoloni.me@gmail.com>
2024-12-11 06:54:33 +0000

    Attempt at solving puzzle 2 part 1 - determine safe reports based on the following criteria:
    - The levels are either all increasing or all decreasing.
    - Any two adjacent levels differ by at least one and at most three.

---
+++ 2/go.mod
@@ -0,0 +1,3 @@
+module github.com/lukepaoloni/advent-of-code-2024/2
+
+go 1.23.4
---
+++ 2/run.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+
+	if err != nil {
+		fmt.Println("Error opening input.txt: ", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	safeReports := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		levels := strings.Split(line, " ")
+		safe := false
+
+		firstLevel, err := strconv.Atoi(levels[0])
+
+		if err != nil {
+			fmt.Println("Error parsing first level as integer.")
+			continue
+		}
+
+		secondLevel, err := strconv.Atoi(levels[1])
+
+		if err != nil {
+			fmt.Println("Error parsing second level as integer.")
+			continue
+		}
+
+		diff := secondLevel - firstLevel
+		allIncreasing := diff > 0
+		safe = diff >= 1 && diff <= 3
+
+		for i := 1; i < len(levels)-1 && safe; i++ {
+			level, err := strconv.Atoi(levels[i])
+
+			if err != nil {
+				fmt.Println("Error parsing level as integer.")
+				safe = false
+				continue
+			}
+
+			nextLevel, err := strconv.Atoi(levels[i+1])
+
+			if err != nil {
+				fmt.Println("Error parsing next level as integer.")
+				safe = false
+				continue
+			}
+
+			diff = nextLevel - level
+
+			increasing := diff > 0
+
+			if !increasing && diff < 0 {
+				diff = level - nextLevel
+			}
+
+			eitherAllIncreasingOrDecreasing := allIncreasing && increasing || !allIncreasing && !increasing
+
+			if !eitherAllIncreasingOrDecreasing {
+				safe = false
+			}
+
+			if diff < 1 || diff > 3 {
+				safe = false
+			}
+		}
+
+		if safe {
+			safeReports = append(safeReports, line)
+		}
+	}
+
+	fmt.Printf("Safe Reports: %d\n", len(safeReports))
+}
